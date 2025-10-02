3887752dca0189215d485011b26c02949744f5d4
Seid Yesuf <seidyesuf750@gmail.com>
2025-10-02 11:04:08 +0300

    docker 2

--- backend/utils/AviaterScraper.js
+++ backend/utils/AviaterScraper.js
@@ -11,10 +11,10 @@ const Scraper=async () => {
   console.log("Chromium executablePath:", executablePath);
 
  const browser = await puppeteer.launch({
-  executablePath: "/usr/bin/chromium",
-  args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  headless: true,
-});
+    executablePath: "/usr/bin/chromium", // Docker Chromium path
+    args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage"],
+    headless: true,
+  });
 
   const page = await browser.newPage();
   await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
