b302a5610bdfc563cd5980fc04d5a47e7373a646
Seid Yesuf <seidyesuf750@gmail.com>
2025-10-13 18:10:09 +0300

    comment

--- backend/utils/grater1.6.js
+++ backend/utils/grater1.6.js
@@ -32,13 +32,13 @@ function processGrater(last30, crashHistory) {
   }
 
   // 5) Stats and conditions
-  const results1 = storedscoreGrater["1.6>"];
-  const diff1 = results1.filter(v => v).length - results1.filter(v => !v).length;
-  const check1 = diff1 > 1 && diff1 < 6 && crashHistory.length > 30;
+  // const results1 = storedscoreGrater["1.6>"];
+  // const diff1 = results1.filter(v => v).length - results1.filter(v => !v).length;
+  // const check1 = diff1 > 1 && diff1 < 6 && crashHistory.length > 30;
 
-  const results2 = storedscoreGrater["1.1<"];
-  const diff2 = results2.filter(v => v).length - results2.filter(v => !v).length;
-  const check2 = diff2 > 1 && diff2 < 6 && crashHistory.length > 30;
+  // const results2 = storedscoreGrater["1.1<"];
+  // const diff2 = results2.filter(v => v).length - results2.filter(v => !v).length;
+  // const check2 = diff2 > 1 && diff2 < 6 && crashHistory.length > 30;
 
   // if (message === "run1.6" && check1) return message;
   // if (message === "run1.1" && check2) return message;
